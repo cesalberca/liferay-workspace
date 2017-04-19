cac23471a864b2279a448a939679ea536b15f824
calberca <cesalberca@gmail.com>
2017-04-19 09:49:06 +0200

    Clean up

--- modules/users-api-rest/src/main/java/com/autentia/api/rest/users/UsersRestService.java
+++ modules/users-api-rest/src/main/java/com/autentia/api/rest/users/UsersRestService.java
@@ -2,10 +2,7 @@ package com.autentia.api.rest.users;
 
 import org.osgi.service.component.annotations.Component;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,6 +10,7 @@ import java.util.Collections;
 import java.util.Set;
 
 @ApplicationPath("/users")
+@Produces(MediaType.APPLICATION_JSON)
 @Component(
 	immediate = true, property = {"jaxrs.application=true"},
 	service = Application.class
@@ -25,12 +23,20 @@ public class UsersRestService extends Application {
 	}
 
 	@GET
-	@Path("/")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response getUsers() {
 		String json = "{\n" + " \"value\": \"ok\"\n" + "}";
 		return Response
 				.status(200)
+				.entity(json)
+				.build();
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response postUser() {
+		String json = "{\n" + " \"value\": \"ok\"\n" + "}";
+		return Response
+				.status(201)
 				.entity(json)
 				.build();
 	}
