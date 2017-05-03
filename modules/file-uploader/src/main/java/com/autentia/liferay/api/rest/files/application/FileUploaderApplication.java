1f686ed97c6c04328c45fd020dabd53a8f05f539
calberca <cesalberca@gmail.com>
2017-05-03 18:04:05 +0200

    Using multipart to get the file

--- modules/file-uploader/src/main/java/com/autentia/liferay/api/rest/files/application/FileUploaderApplication.java
+++ modules/file-uploader/src/main/java/com/autentia/liferay/api/rest/files/application/FileUploaderApplication.java
@@ -6,23 +6,28 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import javax.activation.DataHandler;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 import javax.ws.rs.core.Response.Status;
 import java.io.*;
 import java.util.Collections;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 @ApplicationPath("/files")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Component(immediate = true, service = Application.class)
 public class FileUploaderApplication extends Application {
+
+    public static final String UPLOAD_FILE_SERVER = "/Users/calberca/Desktop";
+    private static final Pattern COMPILE = Pattern.compile("\"");
 
     @Reference
     private volatile DLFileEntryLocalService dlFileEntryLocalService;
@@ -36,41 +41,68 @@ public class FileUploaderApplication extends Application {
     }
 
     @POST
-    public Response postFile(@QueryParam("title") String title, @QueryParam("description") String description) {
-        final File file = new File("/Users/calberca/Desktop/port_v2.png");
-        try (InputStream inputStream = new FileInputStream(file)) {
-            log.info(file.getName());
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response postFile(Iterable<Attachment> attachments, HttpServletRequest request, @QueryParam("title") String title, @QueryParam("description") String description) {
+        for (Attachment attachment : attachments) {
+            final DataHandler dataHandler = attachment.getDataHandler();
 
-            //https://web.liferay.com/community/forums/-/message_boards/message/17070585
-            dlFileEntryLocalService.addFileEntry(
-                    20164,
-                    20147,
-                    20147,
-                    30302,
-                    file.getName(),
-                    MimeTypesUtil.getContentType(file),
-                    title,
-                    description,
-                    null,
-                    0,
-                    null,
-                    file,
-                    inputStream,
-                    file.length(),
-                    new ServiceContext()
-            );
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-            log.info("xxx1 " + e);
-            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-        } catch (IOException e) {
-            log.info("xxx2 " + e);
-            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-        } catch (PortalException e) {
-            log.info("xxx3 " + e);
-            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+            try{
+                final MultivaluedMap<String, String> multivaluedMap = attachment.getHeaders();
+                final String fileName = getFileName(multivaluedMap);
+                log.info(fileName);
+                final InputStream inputStream = dataHandler.getInputStream();
+                final File file = new File(UPLOAD_FILE_SERVER + fileName);
+                writeToFileServer(inputStream, file, fileName, "Description");
+            } catch(IOException e) {
+                log.info(e);
+            }
         }
-
         return Response.status(Status.CREATED).build();
+    }
+
+    private void writeToFileServer(InputStream inputStream, File file, String title, String description) {
+        try {
+            dlFileEntryLocalService.addFileEntry(
+                20164,
+                20147,
+                20147,
+                30302,
+                file.getName(),
+                MimeTypesUtil.getContentType(file),
+                title,
+                description,
+                null,
+                0,
+                null,
+                file,
+                inputStream,
+                file.length(),
+                new ServiceContext()
+            );
+        } catch (PortalException e) {
+            log.info(e);
+            throw new RuntimeException(e);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    log.info(e);
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private String getFileName(MultivaluedMap<String, String> multivaluedMap) {
+        final String[] contentDisposition = multivaluedMap.getFirst("Content-Disposition").split(";");
+        for (String filename : contentDisposition) {
+
+            if (filename.trim().startsWith("filename")) {
+                final String[] name = filename.split("=");
+                return COMPILE.matcher(name[1].trim()).replaceAll("");
+            }
+        }
+        return "unknownFile";
     }
 }
