296a22a4f9d49a6a8275bc5b7ea30542111ae31d
calberca <cesalberca@gmail.com>
2017-05-10 13:01:06 +0200

    Added

--- modules/api-rest-users/src/main/java/com/autentia/liferay/api/rest/users/application/UserMessageBodyWriter.java
+++ modules/api-rest-users/src/main/java/com/autentia/liferay/api/rest/users/application/UserMessageBodyWriter.java
@@ -1,4 +1,32 @@
 package com.autentia.liferay.api.rest.users.application;
 
-public class MessageBodyWriter {
+import com.liferay.portal.kernel.model.User;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+//@Provider
+public class UserMessageBodyWriter implements MessageBodyWriter<User> {
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return User.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public long getSize(User user, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(User user, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        entityStream.write("hi".getBytes());
+    }
 }
