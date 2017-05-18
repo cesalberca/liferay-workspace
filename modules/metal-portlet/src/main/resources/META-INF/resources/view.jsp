a32d0727d20d63fb5c87b2d62721c4996dff7fa3
calberca <cesalberca@gmail.com>
2017-05-18 12:40:10 +0200

    Recreated module from scratch

--- modules/metal-portlet/src/main/resources/META-INF/resources/view.jsp
+++
@@ -1,5 +0,0 @@
-<%@ include file="/init.jsp" %>
-
-<p>
-	<b><liferay-ui:message key="metal-portlet.caption"/></b>
-</p>
---
+++ modules/sample-soy/src/main/resources/META-INF/resources/SoySample.es.js
@@ -0,0 +1,16 @@
+import Component from 'metal-component/src/Component';
+import core from 'metal/src/core';
+import dom from 'metal-dom/src/dom';
+import Soy from 'metal-soy/src/Soy';
+import templates from './SoySample.soy';
+
+class SoySample extends Component {
+    donate(event) {
+        console.log("donate successful");
+    }
+}
+
+//Register component
+Soy.register(SoySample, templates);
+
+export default SoySample;
