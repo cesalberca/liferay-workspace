e66e0507b45e312d207dafcc85c33564c0f93780
calberca <cesalberca@gmail.com>
2017-05-24 10:13:53 +0200

    Imported guava to test wether compileInclude works

--- modules/payrolls/src/main/java/com/autentia/liferay/rest/payrolls/application/PayrollsApplication.java
+++ modules/payrolls/src/main/java/com/autentia/liferay/rest/payrolls/application/PayrollsApplication.java
@@ -1,14 +1,13 @@
 package com.autentia.liferay.rest.payrolls.application;
 
+import com.google.common.collect.ImmutableSet;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.osgi.service.component.annotations.Component;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.POST;
 import javax.ws.rs.core.Application;
-import java.io.InputStream;
 import java.util.Collections;
 import java.util.Set;
 
@@ -24,9 +23,22 @@ public class PayrollsApplication extends Application {
     }
 
     @POST
-    public String postFile(Attachment file) {
-        final InputStream inputStream = file.getObject(InputStream.class);
-        log.info(inputStream.toString());
-        return "It works!";
+    public String postFile() {
+//        try (InputStream inputStream = file.getObject(InputStream.class)) {
+//            log.info(inputStream.toString());
+//            return "It works!";
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return "It doesn't work";
+//        }
+        final ImmutableSet<String> COLOR_NAMES = ImmutableSet.of(
+                "red",
+                "orange",
+                "yellow",
+                "green",
+                "blue",
+                "purple"
+        );
+    return String.valueOf(COLOR_NAMES.size());
     }
 }
